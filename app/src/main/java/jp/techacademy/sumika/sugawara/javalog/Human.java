90022fd3f74a0bf41a24ac08b481f4fcf57b5e29
sumika-sugawara <ssugawar@7andinm.co.jp>
2017-12-21 13:44:12 +0900

    ファイルを修正しました。

--- app/src/main/java/jp/techacademy/sumika/sugawara/javalog/Human.java
+++ app/src/main/java/jp/techacademy/sumika/sugawara/javalog/Human.java
@@ -6,6 +6,7 @@ class Human extends Animal implements Thinkable{
 
     // クラス変数
     static String to_jp = "人";
+    String hobby; //趣味
 
     // コンストラクタ
     public Human(String name, int age, String hobby) {
@@ -16,13 +17,17 @@ class Human extends Animal implements Thinkable{
 
     // クラス関数
     public static void introduce() {
+        Log.d("javatest","これは人クラスです。");
     }
 
     // メンバ関数
     public void say() {
+        Log.d("javatest", "私の名前は" + this.name + "です。年は" + this.age + "歳です。");
     }
 
     @Override
     public void think(){
+        Log.d("javatest", "私は" + this.hobby + "について考える");
+
     }
 }
--- app/src/main/java/jp/techacademy/sumika/sugawara/javalog/MainActivity.java
+++ app/src/main/java/jp/techacademy/sumika/sugawara/javalog/MainActivity.java
@@ -11,11 +11,11 @@ package jp.techacademy.sumika.sugawara.javalog;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Human human = new Human("太郎", 15,"旅行");     // 名前を太郎、年齢15歳で、Humanのインスタンスを作る
+        Human human = new Human("太郎",15,"旅行");
 
         human.say();
-        Log.d("javatest","私の名前は" + human.name + "です。");
-        Log.d("javatest","年は" + human.age + "です。");
-        Log.d("javatest","私は" + human.hobby +"について考える。");
+
+        human.think();
+
     }
 }
